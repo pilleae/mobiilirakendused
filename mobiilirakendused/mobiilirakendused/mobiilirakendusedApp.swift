a247892884349f3261bea5e32dcbda5b03e27078
Pille Allvee <70939481+pilleae@users.noreply.github.com>
2023-03-07 13:18:54 +0200

    setup + toggle heart

---
+++ mobiilirakendused/mobiilirakendused/mobiilirakendusedApp.swift
@@ -0,0 +1,17 @@
+//
+//  mobiilirakendusedApp.swift
+//  mobiilirakendused
+//
+//  Created by Pille Allvee on 07.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct mobiilirakendusedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
